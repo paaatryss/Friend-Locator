39d2c77c65f53ee30845720be346af3117aa5797
Patryk <paaatryss@gmail.com>
2012-11-12 23:00:43 +0100

    Revert "Revert "first version""
    This reverts commit 26e602f4e17bf48b5fa422621632faeb14cd1a8f.

---
+++ app/FriendLocator/src/com/firma/friendlocator/OknoLogowaniaActivity.java
@@ -0,0 +1,34 @@
+package com.firma.friendlocator;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class OknoLogowaniaActivity extends Activity {
+
+	final EditText loginField = (EditText) findViewById(R.id.Login);  
+	String login = loginField.getText().toString();  
+	final EditText passwordField = (EditText) findViewById(R.id.Haslo);  
+	String haslo = passwordField.getText().toString();   
+	final CheckBox responseCheckbox = (CheckBox) findViewById(R.id.ZapamietajLogin);  
+	boolean zapamietajLogin = responseCheckbox.isChecked();
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.okno_logowania);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.okno_logowania, menu);
+        return true;
+    }
+    
+    public void login(View button) {
+    	
+    }
+}
