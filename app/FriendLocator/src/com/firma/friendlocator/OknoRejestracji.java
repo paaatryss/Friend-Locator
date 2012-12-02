a98d39294d450c1b5ec64cf5c091265dbe21284d
Patryk <paaatryss@gmail.com>
2012-12-02 23:49:20 +0100

    **RECOVERY v.1.0.0** Część przywrócona

--- app/FriendLocator/src/com/firma/friendlocator/OknoRejestracji.java
+++ app/FriendLocator/src/com/firma/friendlocator/OknoRejestracji.java
@@ -1,5 +1,7 @@
 package com.firma.friendlocator;
 
+import com.example.friend_locator.R;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
@@ -7,19 +9,19 @@ import android.widget.EditText;
 
 public class OknoRejestracji extends Activity {
 
-	final EditText loginField = (EditText) findViewById(R.id.Login);  
+	/** final EditText loginField = (EditText) findViewById(R.id.Login);  
 	String login = loginField.getText().toString();  
 	final EditText passwordField = (EditText) findViewById(R.id.Haslo);  
 	String haslo = passwordField.getText().toString();   
 	final EditText emailField = (EditText) findViewById(R.id.Haslo);  
 	String email = emailField.getText().toString();
 	final EditText email2Field = (EditText) findViewById(R.id.Haslo);  
-	String email2 = email2Field.getText().toString();
+	String email2 = email2Field.getText().toString(); **/
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.okno_rejestracji);
+        setContentView(R.layout.activity_okno_rejestracji);
     }
     
     public void login(View button) {
