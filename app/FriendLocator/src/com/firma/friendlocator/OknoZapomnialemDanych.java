90c1e0379e6b9ad06936eb6c5bc339d1be3cc191
Patryk <paaatryss@gmail.com>
2012-12-02 23:56:54 +0100

    v2

---
+++ app/FriendLocator/src/com/firma/friendlocator/OknoZapomnialemDanych.java
@@ -0,0 +1,16 @@
+package com.firma.friendlocator;
+
+import com.example.friend_locator.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OknoZapomnialemDanych extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_okno_zapomnialem_danych);
+
+	}
+}
