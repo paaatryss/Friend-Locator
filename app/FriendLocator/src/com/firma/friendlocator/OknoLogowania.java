7d820026aa9a3d72c1ea3efee9715b3d54496d3b
andrzej <andplutecki@gmail.com>
2012-12-04 14:17:10 +0100

    Dodano obsługe logowania i rejestracji.

--- app/FriendLocator/src/com/firma/friendlocator/OknoLogowania.java
+++ app/FriendLocator/src/com/firma/friendlocator/OknoLogowania.java
@@ -5,13 +5,16 @@ import com.firma.friendlocator.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class OknoLogowania extends Activity {
+	Context context;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +27,29 @@ public class OknoLogowania extends Activity {
 	}
 	
 	public void zaloguj(View view) {
+		int check;
 		EditText loginField = (EditText) findViewById(R.id.text_login);  
     	String login = loginField.getText().toString(); 
     	final EditText passwordField = (EditText) findViewById(R.id.text_haslo);  
     	String haslo = passwordField.getText().toString();   
-	    Intent intent = new Intent(this, OknoMenu.class);
-	    startActivity(intent);
+    	check = Auth.login(login,haslo);
+    	if(check==0){
+    		Intent intent = new Intent(this, OknoMenu.class);
+    	    startActivity(intent);
+    	}
+    	else if(check == 1){
+    		context = getApplicationContext();
+    		Toast.makeText(context, "niepopoprawny login bπdü has≥o!", Toast.LENGTH_LONG).show();
+    		
+    	}
+    	else if(check == 2){
+    		context = getApplicationContext();
+    		Toast.makeText(context, "b≥πs po≥πczenia!", Toast.LENGTH_LONG).show();
+    	}
+    	else{
+    		context = getApplicationContext();
+    		Toast.makeText(context, "error "+check +"!", Toast.LENGTH_LONG).show();
+    	}
 	}
         
 	public void zarejestruj(View view) {
--- app/FriendLocator/src/com/firma/friendlocator/OknoRejestracji.java
+++ app/FriendLocator/src/com/firma/friendlocator/OknoRejestracji.java
@@ -4,12 +4,16 @@ import com.firma.friendlocator.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class OknoRejestracji extends Activity {
+	Context context;
 
 	/*EditText loginField = (EditText) findViewById(R.id.text_login_rejestracja);  
 	String login = loginField.getText().toString();  
@@ -30,7 +34,30 @@ public class OknoRejestracji extends Activity {
     }
     
     public void login(View button) {
+    	int check;
     	EditText loginField = (EditText) findViewById(R.id.text_login_rejestracja);  
     	String login = loginField.getText().toString(); 
+    	final EditText passwordField = (EditText) findViewById(R.id.text_haslo_rejestracja);  
+    	String haslo = passwordField.getText().toString();
+    	final EditText emailField = (EditText) findViewById(R.id.text_email_rejestracja);  
+    	String email = emailField.getText().toString();
+    	check = Auth.register(login,haslo,email);
+    	if(check==0){
+    		Intent intent = new Intent(this, OknoMenu.class);
+    	    startActivity(intent);
+    	}
+    	else if(check == 1){
+    		context = getApplicationContext();
+    		Toast.makeText(context, "niepopoprawny login bπdü has≥o!", Toast.LENGTH_LONG).show();
+    		
+    	}
+    	else if(check == 2){
+    		context = getApplicationContext();
+    		Toast.makeText(context, "b≥πs po≥πczenia!", Toast.LENGTH_LONG).show();
+    	}
+    	else{
+    		context = getApplicationContext();
+    		Toast.makeText(context, "error "+check +"!", Toast.LENGTH_LONG).show();
+    	}
     }
 }
