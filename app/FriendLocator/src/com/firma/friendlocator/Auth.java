d63455bc1ca4245b47fb10bbf0e145417b2b4b15
andrzej <andplutecki@gmail.com>
2012-12-04 16:04:38 +0100

    klasa autoryzacji v.1

---
+++ app/FriendLocator/src/com/firma/friendlocator/Auth.java
@@ -0,0 +1,10 @@
+package com.firma.friendlocator;
+
+class Auth {
+	static public int login(String login,String haslo){
+		return 0;
+	}
+	static public int register(String login,String haslo,String email){
+		return 0;
+	}
+}
