d09dbed4cd72c164d416cb652247413d4b96786d
andrzej <andplutecki@gmail.com>
2013-01-16 19:50:03 +0100

    -yoyoyo

--- app/FriendLocator/src/com/firma/friendlocator/OknoRejestracji.java
+++ app/FriendLocator/src/com/firma/friendlocator/OknoRejestracji.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -24,6 +26,7 @@ public class OknoRejestracji extends Activity {
         setContentView(R.layout.activity_okno_rejestracji);
     }
     int check;
+	String checka;
     public void login(View button) {  
     	EditText loginField = (EditText) findViewById(R.id.text_login_rejestracja);
     	final EditText passwordField = (EditText) findViewById(R.id.text_haslo_rejestracja);
@@ -31,11 +34,23 @@ public class OknoRejestracji extends Activity {
     	String login = loginField.getText().toString();  
     	String haslo = passwordField.getText().toString();     
     	String email = emailField.getText().toString(); 
-    	//check = Auth.register(login,haslo,email);
-    	check=0;
+    	check = Auth.register(login,haslo,email);
     	if(check==0){
-    		Intent intent = new Intent(this, OknoMenu.class);
-    	    startActivity(intent);
+        	checka = Auth.login(login,haslo);
+        	//Log.d("info", "login zwraca:"+check);
+        	if(checka.equals("1")){
+        		context = getApplicationContext();
+        		Toast.makeText(context, "niepopoprawny login bπdü has≥o!", Toast.LENGTH_LONG).show();
+        		
+        	}
+        	else if(checka.equals("2")){
+        		context = getApplicationContext();
+        		Toast.makeText(context, "b≥πs po≥πczenia!", Toast.LENGTH_LONG).show();
+        	}
+        	else{
+        		Intent intent = new Intent(this, OknoMenu.class);
+        	    startActivity(intent);
+        	}
     	}
     	else if(check == 1){
     		context = getApplicationContext();
