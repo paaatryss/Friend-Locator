2f11a5286b7447bf6d361ad587eb10def7e4548c
Leszek <leszek.foltman@gmail.com>
2013-01-16 19:25:21 +0100

    rejestracja fix

--- app/FriendLocator/src/com/firma/friendlocator/Auth.java
+++ app/FriendLocator/src/com/firma/friendlocator/Auth.java
@@ -22,7 +22,7 @@ public class Auth {
 	static public String login(String login,String haslo){
 		 StringBuilder builder = new StringBuilder();
 		    HttpClient client = new DefaultHttpClient();
-		    Log.d("info:", "0");
+		    Log.d("login:", "0");
 		    HttpGet httpGet = new HttpGet("http://flyer.kei.pl/friendlocator/login.php?login=" + login + "&password=" + haslo);
 		    try {
 			      HttpResponse response = client.execute(httpGet);
@@ -45,25 +45,63 @@ public class Auth {
 			      e.printStackTrace();
 			    }
 			    try{
-			    	Log.d("info:", "1");
+			    	Log.d("login:", "1");
 			    	JSONObject jObject = new JSONObject(builder.toString());
-			    	Log.d("info:", "3");
+			    	Log.d("login:", "3");
 			    	int error = jObject.optInt("error");
-			    	Log.d("info:", "error: " + error);
+			    	Log.d("login:", "error: " + error);
 			    	if(error != 1)
 			    	{
-			    		Log.d("info:", "4");
+			    		Log.d("login:", "4");
 			    		String token = jObject.getString("token");
-			    		Log.d("info:", "5");
+			    		Log.d("login:", "5");
 			    				return token;
 			    	}
-			    	Log.d("info:", "6");
+			    	Log.d("login:", "6");
 			    }catch(JSONException e){
 			    		return "1";
 			    }
 			  return "1";
 	}
 	static public int register(String login,String haslo,String email){
-		return 0;
+		 StringBuilder builder = new StringBuilder();
+		    HttpClient client = new DefaultHttpClient();
+		    Log.d("register:", "0");
+		    HttpGet httpGet = new HttpGet("http://flyer.kei.pl/friendlocator/register.php?login=" + login + "&password=" + haslo + "&email=" + email);
+		    try {
+			      HttpResponse response = client.execute(httpGet);
+			      StatusLine statusLine = response.getStatusLine();
+			      int statusCode = statusLine.getStatusCode();
+   		      if (statusCode == 200) {
+   		        HttpEntity entity = response.getEntity();
+   		        InputStream content = entity.getContent();
+   		        BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+   		        String line;
+   		        while ((line = reader.readLine()) != null) {
+   		          builder.append(line + "\n");
+   		        }
+   		      } else {
+   		        	return 2;
+   		      }
+			    } catch (ClientProtocolException e) {
+			      e.printStackTrace();
+			    } catch (IOException e) {
+			      e.printStackTrace();
+			    }
+			    try{
+			    	Log.d("register:", "1");
+			    	JSONObject jObject = new JSONObject(builder.toString());
+			    	Log.d("register:", "3");
+			    	int error = jObject.optInt("error");
+			    	Log.d("register:", "error: " + error);
+			    	if(error != 1)
+			    	{
+			    		return 0;
+			    	}
+			    	Log.d("register:", "6");
+			    }catch(JSONException e){
+			    		return 1;
+			    }
+			  return 1;
 	}
 }
